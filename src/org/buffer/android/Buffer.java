dcca9bf020729586789ebf595986b8738b4e652e
aimee daniells <aimee@edendevelopment.co.uk>
2011-05-19 22:38:58 +0100

    Ooo, we have a menu. We can now quit the app! :)

--- src/org/buffer/android/Buffer.java
+++ src/org/buffer/android/Buffer.java
@@ -2,6 +2,9 @@ package org.buffer.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Buffer extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,23 @@ public class Buffer extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu, menu);
+    	return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()) {
+    	case R.id.quit:
+    		finish();
+    		break;
+    	}
+    	return false;
     }
 }
