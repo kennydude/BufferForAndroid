417b745ecd563965f03fdcd3fe1d8d3a494192ed
aimee daniells <aimee@edendevelopment.co.uk>
2011-05-21 22:02:59 +0100

    Some sort of content for the home view.

--- src/org/buffer/android/Buffer.java
+++ src/org/buffer/android/Buffer.java
@@ -1,18 +1,32 @@
 package org.buffer.android;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.security.NoSuchAlgorithmException;
+
+import org.json.JSONException;
+
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.util.Log;
 
 public class Buffer extends Activity {
+
+	private static final String TAG = "Buffer";
+	TextView display;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        display = (TextView) this.findViewById(R.id.display);
+        UpdateContent();
     }
 
     @Override
@@ -36,4 +50,20 @@ public class Buffer extends Activity {
     	}
     	return false;
     }
+
+	public void UpdateContent() {
+		try {
+			ProcessResponse(SearchRequest("android"));
+		} catch(Exception e) {
+			Log.v(TAG, "Exception: " + e.getMessage());
+		}
+	}
+
+	public String SearchRequest(String searchString) throws MalformedURLException, IOException {
+		return "existing buffered tweets will come here";
+	}
+
+	public void ProcessResponse(String resp) throws IllegalStateException, IOException, JSONException, NoSuchAlgorithmException {
+		display.setText(resp);
+	}
 }
