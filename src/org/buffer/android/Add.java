8b35ada0493c9a5581d61a17cfd1efb12a3215a3
aimee daniells <aimee@edendevelopment.co.uk>
2011-05-21 21:18:28 +0100

    Tidy the code a little.

--- src/org/buffer/android/Add.java
+++ src/org/buffer/android/Add.java
@@ -5,28 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.Button;
 import android.util.Log;
 
 public class Add extends Activity implements OnClickListener {
 
 	private static final String TAG = "Buffer";
+	EditText tweetTextView;
+	Button addButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add);
 
+		tweetTextView = (EditText) this.findViewById(R.id.tweet_text);
+
 		// Click listener
-		View addButton = this.findViewById(R.id.add_button);
+		addButton = (Button) this.findViewById(R.id.add_button);
 		addButton.setOnClickListener(this);
 	}
 
 	public void onClick(View v) {
 		switch(v.getId()) {
 		case R.id.add_button:
-			EditText tweetTextView = (EditText) this.findViewById(R.id.tweet_text);
-			String tweetText = tweetTextView.getText().toString();
-			Log.d(TAG, tweetText);
+			Log.d(TAG, tweetTextView.getText().toString());
 			this.finish();
 			break;
 		}
